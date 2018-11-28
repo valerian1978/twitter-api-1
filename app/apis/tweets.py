07029361eb1312f743919030be2a5b9b54e98761
Sébastien Saunier <seb@saunier.me>
2018-11-28 23:27:43 +0100

    CRUD on tweets with SQLAlchemy

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -1,7 +1,7 @@
 from flask_restplus import Namespace, Resource, fields
 from flask import abort
-from app.db import tweet_repository
 from app.models import Tweet
+from app import db
 
 api = Namespace('tweets')
 
@@ -21,7 +21,7 @@ json_new_tweet = api.model('New tweet', {
 class TweetResource(Resource):
     @api.marshal_with(json_tweet)
     def get(self, id):
-        tweet = tweet_repository.get(id)
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
             api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
@@ -30,7 +30,7 @@ class TweetResource(Resource):
     @api.marshal_with(json_tweet, code=200)
     @api.expect(json_new_tweet, validate=True)
     def patch(self, id):
-        tweet = tweet_repository.get(id)
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
             api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
@@ -38,11 +38,12 @@ class TweetResource(Resource):
             return tweet
 
     def delete(self, id):
-        tweet = tweet_repository.get(id)
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
             api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
-            tweet_repository.remove(id)
+            db.session.delete(tweet)
+            db.session.commit()
             return None
 
 @api.route('')
@@ -53,8 +54,9 @@ class TweetsResource(Resource):
     def post(self):
         text = api.payload["text"]
         if len(text) > 0:
-            tweet = Tweet(text)
-            tweet_repository.add(tweet)
+            tweet = Tweet(text=text)
+            db.session.add(tweet)
+            db.session.commit()
             return tweet, 201
         else:
             return abort(422, "Tweet text can't be empty")
